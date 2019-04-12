f3a4b4d89cde3e348ded71cb255832eab1f66e19
Dmitry <dimasta00@gmail.com>
2019-04-12 15:38:50 +0300

    Add header file for Circle.cpp

---
+++ Circle.h
@@ -0,0 +1,33 @@
+//
+// Created by dimas on 4/12/19.
+//
+
+#ifndef CURVE_CIRCLE_H
+#define CURVE_CIRCLE_H
+
+
+#include "Point.h"
+#include "Curve.h"
+#include <cmath>
+
+class Circle : Curve {
+private:
+    Point center;
+    double radius{};
+
+
+public:
+    Circle() : Curve() {}
+
+    Circle(Point ccenter, double r);
+
+    Point Value(double t) override;
+
+    bool isClosed() override;
+
+    Point tangent(double t) override;
+
+};
+
+
+#endif //CURVE_CIRCLE_H

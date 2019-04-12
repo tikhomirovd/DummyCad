8f16bb16fa2e32ff529cef18a79f0828bcaf0b7e
Dmitry <dimasta00@gmail.com>
2019-04-12 15:44:57 +0300

    Add header file for Point

---
+++ Point.h
@@ -0,0 +1,56 @@
+//
+// Created by dimas on 4/12/19.
+//
+
+#ifndef CURVE_POINT_H
+#define CURVE_POINT_H
+
+#include <iostream>
+
+using namespace std;
+
+
+class Point {
+private:
+    double _x, _y, _z;
+
+
+public:
+    Point();
+
+    explicit Point(double);
+
+    Point(double, double);
+
+    Point(double, double, double);
+
+    void setCoord(double x, double y, double z);
+
+    double x();
+
+    double y();
+
+    double z();
+
+    void getCoord();
+
+    friend Point operator+(Point &a, Point &b) {
+
+        return {a.x() + b.x(), a.y() + b.y(), a.z() + b.z()};
+    }
+
+    friend const Point operator*(Point &a, const double &i) {
+
+        return {a.x() * i, a.y() * i, a.z() * i};
+    }
+
+    friend const Point operator+(Point &a, const double &i) {
+
+        return {a.x() + i, a.y() + i, a.z() + i};
+    }
+
+
+};
+
+
+#endif //CURVE_POINT_H
